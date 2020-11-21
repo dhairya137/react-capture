5ace8296ffd7a7c7a612fcfc54e14dc1b259ee08
Dhairya <dhairya00798@gmail.com>
2020-11-21 12:57:54 +0530

    about section

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// IMport page containers
+import AboutSection from '../components/AboutSection';
+
+const Aboutus = () => {
+  return (
+    <>
+      <AboutSection />
+    </>
+  )
+}
+
+export default Aboutus;
