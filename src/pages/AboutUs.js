625f81d5da154fb294a3a4956b1d9191d23fcc92
Dhairya <dhairya00798@gmail.com>
2020-11-21 13:05:39 +0530

    services and faq sections done

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,11 +1,15 @@
 import React from 'react'
 // IMport page containers
 import AboutSection from '../components/AboutSection';
+import ServicesSection from '../components/ServicesSection';
+import FaqSection from '../components/FaqSection'
 
 const Aboutus = () => {
   return (
     <>
       <AboutSection />
+      <ServicesSection/>
+      <FaqSection />
     </>
   )
 }
